b19b5f272afd38a36659bcc02ff95e4ab99c4b36
Nicolas Bouvrette <bouvrette.nicolas@gmail.com>
2017-07-26 21:17:36 -0400

    Update svgFallback.js
    Removed the IE specific code to make this cross-browser compatible and added a generic URL function.

--- svgFallback.js
+++ svgFallback.js
@@ -1,5 +1,5 @@
 /**
- * svgfallback.js 1.4
+ * svgfallback.js 1.5
  *
  * Copyright 2017, Nicolas Bouvrette http://ca.linkedin.com/in/nicolasbouvrette/
  * Released under the WTFPL license - http://www.wtfpl.net/
@@ -26,68 +26,93 @@ window.svgFallback = {
     /** @property String - The extension of the fallback image file when SVG is not supported. */
     fallbackExtension: 'png',
     /** @property RegExp - Regular expression used to find and replace SVG images in image URLs. */
-    imgSrcRegExp: /^(.+)(\.svg)(\?.)*$/ig,
+    urlRegExp: /^(.+)\.svg(\?.*)?$/ig,
     /** @property RegExp - Regular expression used to find and replace SVG images in stylesheets. */
-    stylesheetRegExp: /([\w]+)(\.svg)([')"])/ig,
+    styleUrlRegExp: /(.*url\()(.*)\.svg((\?.*)?["']?\).*)/ig,
 
     /**
-     * Is the element an image of type SVG?
+     * Fallback a given URL to the supported extension.
      *
-     * @param {Object} element - The HTML element.
+     * @param {string} url                                        - The HTML element.
+     * @param {string} [fallbackExtension=this.fallbackExtension] - the extension of the fall back image files.
      *
-     * @returns {Boolean} True when the element is an image of type SVG, otherwise false.
+     * @returns {string} The URL with the supported extension.
      */
-    isSvgImg: function (element) {
-        return this.imgSrcRegExp.test(element.src);
+    fallbackUrl: function (url, fallbackExtension) {
+        fallbackExtension = typeof fallbackExtension !== 'undefined' ? fallbackExtension : this.fallbackExtension;
+        if (typeof url === 'string' && this.urlRegExp.test(url)) {
+            return url.replace(this.urlRegExp, '$1.' + fallbackExtension + '$2');
+        }
+        return url;
     },
 
     /**
      * Fallback a given HTML image element to the supported extension.
      *
      * @param {HTMLElement|HTMLImageElement} element              - The HTML element.
-     * @param {String} [fallbackExtension=this.fallbackExtension] - the extension of the fall back image files.
+     * @param {string} [fallbackExtension=this.fallbackExtension] - the extension of the fall back image files.
      */
     fallbackImgSrc: function (element, fallbackExtension) {
         fallbackExtension = typeof fallbackExtension !== 'undefined' ? fallbackExtension : this.fallbackExtension;
-        if (this.isSvgImg(element)) {
-            element.src = element.src.replace(this.imgSrcRegExp, '$1.' + fallbackExtension + '$3');
+        element.src = this.fallbackUrl(element.src, fallbackExtension);
+    },
+
+    /**
+     * Fallback a all stylesheets using SVG images to use the supported extension.
+     *
+     * @param {string} [fallbackExtension=this.fallbackExtension] - the extension of the fall back image files.
+     */
+    fallbackStyleSheets: function (fallbackExtension) {
+        fallbackExtension = typeof fallbackExtension !== 'undefined' ? fallbackExtension : this.fallbackExtension;
+        var styleSheets = document.styleSheets;
+        for (var styleSheetId = 0; styleSheetId < styleSheets.length; styleSheetId++) {
+
+            var styleSheet = styleSheets[styleSheetId];
+            var rules = (styleSheet.cssRules) ? styleSheet.cssRules : styleSheet.rules;
+
+            for (var ruleId = 0; ruleId < rules.length; ruleId++) {
+                if (typeof rules[ruleId] !== 'undefined' && typeof rules[ruleId].style !== 'undefined') {
+                    var rule = rules[ruleId];
+                    this.fallbackStyleUrl(rule.style, 'background', fallbackExtension);
+                    this.fallbackStyleUrl(rule.style, 'backgroundImage', fallbackExtension);
+                }
+            }
         }
     },
 
     /**
      * Fallback a all stylesheets using SVG images to use the supported extension.
      *
-     * @param {String} [fallbackExtension=this.fallbackExtension] - the extension of the fall back image files.
+     * @param {CSSStyleDeclaration} style                         - the style declaration to fallback.
+     * @param {string} property                                   - the style property to fallback.
+     * @param {string} [fallbackExtension=this.fallbackExtension] - the extension of the fall back image files.
      */
-    fallbackStyleSheets: function (fallbackExtension) {
+    fallbackStyleUrl: function (style, property, fallbackExtension) {
         fallbackExtension = typeof fallbackExtension !== 'undefined' ? fallbackExtension : this.fallbackExtension;
-        var styleSheets = document.styleSheets;
-        for (var iterator = 0; iterator < styleSheets.length; iterator++) {
-            var styleSheet = styleSheets[iterator];
-            if (this.stylesheetRegExp.test(styleSheet.cssText)) {
-                styleSheet.cssText = styleSheet.cssText.replace(this.stylesheetRegExp, '$1.' + fallbackExtension + '$3');
-            }
+        if (typeof style[property] === 'string' && style[property].length &&
+            this.styleUrlRegExp.test(style[property])) {
+            style[property] = style[property].replace(this.styleUrlRegExp, '$1$2.' + fallbackExtension + '$3');
         }
     },
 
     /**
      * Fallback all image source SVG references to the supported extension.
      *
-     * @param {String} [fallbackExtension=this.fallbackExtension] - the extension of the fall back image files.
+     * @param {string} [fallbackExtension=this.fallbackExtension] - the extension of the fall back image files.
      */
     fallbackAllImgSrc: function (fallbackExtension) {
         fallbackExtension = typeof fallbackExtension !== 'undefined' ? fallbackExtension : this.fallbackExtension;
-        var elements = document.querySelectorAll('img[src]');
-        for (var iterator = 0; iterator < elements.length; iterator++) {
-            var element = elements[iterator];
-            this.fallbackImgSrc(element, fallbackExtension);
+        var imageElements = document.querySelectorAll('img[src]');
+        for (var imageElementId = 0; imageElementId < imageElements.length; imageElementId++) {
+            var imageElement = imageElements[imageElementId];
+            this.fallbackImgSrc(imageElement, fallbackExtension);
         }
     },
 
     /**
      * Fallback all elements supported by this script.
      *
-     * @param {String} [fallbackExtension=this.fallbackExtension] - the extension of the fall back image files.
+     * @param {string} [fallbackExtension=this.fallbackExtension] - the extension of the fall back image files.
      */
     fallback: function (fallbackExtension) {
         fallbackExtension = typeof fallbackExtension !== 'undefined' ? fallbackExtension : this.fallbackExtension;
